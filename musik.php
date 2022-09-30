9e8d3bf0c6ec5b934a9e78f78b312eaadb8fc692
paisssszz <faizf5729@gmail.com>
2022-09-30 11:35:36 +0700

    Tampilan

--- musik.php
+++ musik.php
@@ -17,10 +17,35 @@
 </head>
 <body>
     <div class="navbar">
-        <div class="logo"><img src="ht.png" alt=""></div>
+        <div class="logo"><img src="ht.png" alt="">
         <ul>
             <li><a href="#">Login</a></li>
         </ul>
+    </div>
+    <div class="gambar1"><img src="image.png" alt=""></div>
+    <div class="foto1"><img src="1.png" alt=""></div>
+    <div class="reggae"><img src="reggae.png" alt=""></div>
+    <div class="rock"><img src="rock.png" alt=""></div>
+    <div class="dangdut"><img src="dangdut.png" alt=""></div>
+    <div class="alterf"><img src="alter.png" alt=""></div>
+    <div class="blues"><img src="blues.png" alt=""></div>
+    <div class="acou"><img src="axoustic.png" alt=""></div>
+    <div class="clasic"><img src="clasical.png" alt=""></div>
+    <div class="rap"><img src="rap.png" alt=""></div>
+    <div class="hardcore"><img src="hardcore.png" alt=""></div>
+    <div class="punk"><img src="punk.png" alt=""></div>
+    <div class="s"><img src="80s.png" alt=""></div>
+    <div class="indie"><img src="indie.png" alt=""></div>
+    <div class="coun"><img src="coun.png" alt=""></div>
+    <div class="jazz"><img src="jazz.png" alt=""></div>
+    <div class="pop"><img src="pop.png" alt=""></div>
+    <div class="rock2"><img src="rock.png" alt=""></div>
+    <div class="hiphop"><img src="hiphop.png" alt=""></div>
+    <div class="rnb"><img src="rnb.png.jpg" alt=""></div>
+    <div class="foto2"><img src="foto2.png" alt=""></div>
+
+    <div class="judul">
+        <p>TOP SONG's</p>
     </div>
 </body>
 </html>
